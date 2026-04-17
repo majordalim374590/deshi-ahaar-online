ee8d3e1b680d83e2a3cdb2b89650b8bb8f1b23c9
majordalim374590 <majordalim374590@gmail.com>
2026-04-18 00:42:08 +0600

    Create User model with MongoDB schema and password hashing

---
+++ backend/models/User.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+// User Schema
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /.+\@.+\..+/,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+}, { timestamps: true });
+
+// Password hashing middleware
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
